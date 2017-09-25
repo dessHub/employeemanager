309062619b24dc34ba9a5f7012841ed17f7e3c9f
dessHub <deskip95@gmail.com>
2017-09-25 13:14:10 +0300

    integrated with angular front end

---
+++ public/app/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('employeeRecords', []) // creates an AngularJS module and assigns the object to the variable app.
+        .constant('API_URL', 'http://localhost:8080/employeemanager/public/');
